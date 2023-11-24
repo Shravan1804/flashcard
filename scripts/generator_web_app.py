e9a05b07dc725221e4386a4ffbac45d193a1190c
shravan1804 <shravan1804@yahoo.fr>
2023-11-24 18:28:45 +0100

    Create background_callback_manager.

--- scripts/generator_web_app.py
+++ scripts/generator_web_app.py
@@ -1,11 +1,17 @@
 import dash_bootstrap_components as dbc
-from dash import Dash
+import diskcache
+from dash import Dash, DiskcacheManager
 
 from src.web_app.callbacks import register_callbacks
 from src.web_app.layouts import get_app_layout
 
 if __name__ == "__main__":
-    app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+    cache = diskcache.Cache("./cache")
+    app = Dash(
+        __name__,
+        external_stylesheets=[dbc.themes.BOOTSTRAP],
+        background_callback_manager=DiskcacheManager(cache),
+    )
     app.layout = get_app_layout()
     register_callbacks()
     app.run(debug=True)
