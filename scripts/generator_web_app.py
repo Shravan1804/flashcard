57f613ee3ed112837a41a20a632b31bc47e87db9
shravan1804 <shravan1804@yahoo.fr>
2023-11-28 11:12:03 +0100

    Env file for openai api key.

--- scripts/generator_web_app.py
+++ scripts/generator_web_app.py
@@ -1,11 +1,13 @@
 import dash_bootstrap_components as dbc
 import diskcache
 from dash import Dash, DiskcacheManager
+from dotenv import load_dotenv
 
 from src.web_app.callbacks import register_callbacks
 from src.web_app.layouts import get_app_layout
 
 if __name__ == "__main__":
+    load_dotenv()
     cache = diskcache.Cache("./cache")
     app = Dash(
         __name__,
