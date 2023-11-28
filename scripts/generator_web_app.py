4d5a02500110f1dd1e2c36a7642d465d75f2757a
shravan1804 <shravan1804@yahoo.fr>
2023-11-28 22:25:18 +0100

    Load env before importing babelnet.

--- scripts/generator_web_app.py
+++ scripts/generator_web_app.py
@@ -3,12 +3,14 @@ import diskcache
 from dash import Dash, DiskcacheManager
 from dotenv import load_dotenv
 
-from src.generator.concept_generator import ConceptGenerator
-from src.web_app.callbacks import register_callbacks
-from src.web_app.layouts import get_app_layout
+load_dotenv()
+
+from src.crawler.babelnet_crawler import BabelNetCrawler  # noqa: E402
+from src.generator.concept_generator import ConceptGenerator  # noqa: E402
+from src.web_app.callbacks import register_callbacks  # noqa: E402
+from src.web_app.layouts import get_app_layout  # noqa: E402
 
 if __name__ == "__main__":
-    load_dotenv()
     cache = diskcache.Cache("./cache")
     app = Dash(
         __name__,
@@ -17,6 +19,7 @@ if __name__ == "__main__":
         assets_folder="../assets",
     )
     app.layout = get_app_layout()
-    concept_generator = ConceptGenerator(20)
-    register_callbacks(concept_generator)
+    concept_generator = ConceptGenerator(10)
+    babelnet_crawler = BabelNetCrawler()
+    register_callbacks(concept_generator, babelnet_crawler)
     app.run(debug=True)
